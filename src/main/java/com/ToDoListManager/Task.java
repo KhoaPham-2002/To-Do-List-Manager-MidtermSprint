61fedcb3eece59ef646da315b4aacfad0c7449cd
Khoa Pham <Khoa.Pham@keyin.com>
2025-06-26 10:39:33 -0400

    Updated Tasklist.java, implement TaskList as a singly linked list to store and manage tasks

---
+++ src/main/java/com/ToDoListManager/Task.java
@@ -0,0 +1,32 @@
+//Name: Khoa Pham
+//Project: Midterm Sprint (To-Do List Manager)
+//Date: 06/26/2025
+
+package src.main.java.com.ToDoListManager;
+public class Task {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public void markAsCompleted() {
+        isCompleted = true;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return description + " [" + (isCompleted ? "Completed" : "Pending") + "]";
+    }
+}
+
